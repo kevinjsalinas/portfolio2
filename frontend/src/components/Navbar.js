b295f906d4aca3ef103838224d6e9beef3571cba
Kevin Salinas <kevinsalinas155@gmail.com>
2023-06-12 19:10:34 -0400

    nav bar setup

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import Logo from '../assets/ks-logo-white-text.png'
+
+
+function Navbar() {
+
+    const [nav, setNav] = useState(false)
+
+
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        <div>
+            <img src={Logo} alt="logo" style={{width:'60px'}}/>
+        </div>
+
+        {/* Menu */}
+        <ul className='hidden md:flex'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Work</li>
+            <li>Contact</li>
+        </ul>
+
+        {/* Hamburger */}
+
+        <div onClick={() => setNav(!nav)} className='md:hidden z-10'>
+            {!nav ? <FaBars/> : <FaTimes/>}
+        </div>
+
+        {/* Mobile Menu */}
+        <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+            <li className='py-6 text-4xl'>Home</li>
+            <li className='py-6 text-4xl'>About</li>
+            <li className='py-6 text-4xl'>Skills</li>
+            <li className='py-6 text-4xl'>Work</li>
+            <li className='py-6 text-4xl'>Contact</li>
+        </ul>
+
+        {/* Social icons */}
+
+
+    </div>
+  )
+}
+
+export default Navbar
