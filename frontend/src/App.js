c2b513480999ee70604d2b95831fc4632a52ab00
Kevin Salinas <kevinsalinas155@gmail.com>
2023-06-15 22:48:40 -0400

    added about me section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
+import Skills from "./components/Skills";
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
     <div>
       <Navbar/>
       <Home/>
+      <About/>
+      <Skills/>
      
     </div>
   );
