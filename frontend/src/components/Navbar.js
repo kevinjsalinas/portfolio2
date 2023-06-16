95ea6b8ba81f875c41d315b94f80d2d2cbdbbae9
Kevin Salinas <kevinsalinas155@gmail.com>
2023-06-16 13:51:49 -0400

    making last edits

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -11,6 +11,7 @@ import Logo from '../assets/ks-logo-white-text.png'
 function Navbar() {
 
     const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
 
 
   return (
@@ -50,36 +51,56 @@ function Navbar() {
 
         {/* Hamburger */}
 
-        <div onClick={() => setNav(!nav)} className='md:hidden z-10'>
+        <div onClick={handleClick} className='md:hidden z-10'>
             {!nav ? <FaBars/> : <FaTimes/>}
         </div>
 
         {/* Mobile Menu */}
         <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-            <li className='py-6 text-4xl'>Home</li>
-            <li className='py-6 text-4xl'>About</li>
-            <li className='py-6 text-4xl'>Skills</li>
-            <li className='py-6 text-4xl'>Work</li>
-            <li className='py-6 text-4xl'>Contact</li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+                    Home
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+                    About
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+                    Skills
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+                    Work
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+                    Contact
+                </Link>
+            </li>
         </ul>
 
         {/* Social icons */}
-        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <div className='hidden lg:flex fixed flex-col top-[35%] left-0 text-white'>
             <ul>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-                    <a className="flex justify-between items-center w-full tex-gray-300" href='/'>
+                    <a className="flex justify-between items-center w-full tex-gray-300" href='https://www.linkedin.com/in/kevinsalinas/'>
                         LinkedIn <FaLinkedin size={30}/>
 
                     </a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-                    <a className="flex justify-between items-center w-full tex-gray-300" href='/'>
+                    <a className="flex justify-between items-center w-full tex-gray-300" href='https://github.com/kevinjsalinas'>
                         Github <FaGithub size={30}/>
 
                     </a>
                 </li>
                 <li className='w-[165px] h-[60px] flex justify-between items-center ml-[-105px] hover:ml-[-10px] duration-300 bg-[#70b48b]'>
-                    <a className="flex justify-between items-center w-full tex-gray-300" href='/'>
+                    <a className="flex justify-between items-center w-full tex-gray-300" href='https://www.hackerrank.com/kevinjsalinas'>
                         HackerRank <SiHackerrank size={30}/>
 
                     </a>
@@ -91,9 +112,12 @@ function Navbar() {
                     </a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#bebebe] '>
-                    <a className="flex justify-between items-center w-full text-slate-100" href='/'>
-                        Email <HiOutlineMail size={30}/>
-                    </a>
+                    <span className="flex justify-between items-center w-full text-slate-100">
+                        <Link to="contact" smooth={true} duration={500}>
+                            Email
+                        </Link>
+                        <HiOutlineMail size={30}/>
+                    </span>
                 </li>
             </ul>
 
