0f6f9eb7089345e271e39896a9773407d6b90189
Kevin Salinas <kevinsalinas155@gmail.com>
2023-06-21 11:33:56 -0400

    added build

--- frontend/src/components/Work.js
+++ frontend/src/components/Work.js
@@ -13,7 +13,7 @@ function Work() {
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-amber-400'>Work</p>
-                <p className='py-6'>/ / Check out some of my recent work</p>
+                <p className='py-6'>/ / Check out some of my recent projects</p>
             </div>
 
             {/* container */}
